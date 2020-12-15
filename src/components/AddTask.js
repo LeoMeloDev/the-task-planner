a25f30a1bee917695136fec9211c4c34cf157a30
LeoMeloDev <contact@leomelo.com>
2020-12-15 16:57:52 +0000

    Added Header, task input and add task button

---
+++ src/components/AddTask.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const AddTask = (props) => {
+    const [inputValue, setInputValue] = React.useState('');
+
+    const handleChange = (event) => setInputValue(event.target.value);
+    const handleAddTask = () => {
+        props.addToDo(inputValue)
+        setInputValue('')
+    }    
+    return (
+        <div className="add__task">
+            <input type="text" value={inputValue} placeholder={props.placeholder} onChange={ handleChange }/>
+            <button onClick={handleAddTask}>add task</button>
+        </div>
+        
+    )
+}
+
+export default AddTask
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>the task planner</h1>
+        </header>  
+    )
+}
+
+export default Header
